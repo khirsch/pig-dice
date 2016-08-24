1ada84f5479ceed0d73abf2d26e4fa7fc38822db
Katrina Hirsch and Mark Lawson <katrina@>
2016-08-24 09:44:43 -0700

    elaborate on business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,54 @@
 // business logic
 
-function Player(name, score) {
-  this.name = name;
+function Game(currentPlayer, gameOver) {
+  this.currentPlayer = currentPlayer;
+  this.gameOver = gameOver;
+}
+
+function Player(score) {
   this.score = score;
 }
 
+var turnTotal = 0;
+
+function startGame() {
+  var game = new Game("player1", false);
+  var player1 = new Player(0);
+  var player2 = new Player(0);
+}
+
+function endGame() {
+
+}
+
 function roll() {
-  return Math.floor(Math.random()*6)+1;
+  var roll = Math.floor(Math.random()*6)+1;
+  if (roll > 1) {
+    turnTotal += roll;
+  } else {
+    turnTotal = 0;
+    if (game.currentPlayer === "player1") {
+      player1.score += turnTotal;
+      if (player1.score >= 100) {
+        endGame();
+      }
+      game.currentPlayer = "player2";
+    } else {
+      player2.score += turnTotal;
+      game.currentPlayer = "player1";
+      if (player2.score >= 100) {
+        endGame();
+      }
+    }
+  }
+}
+
+function updatePlayerScore() {
+
 }
 
 // user interface logic
 $(function() {
+
+
 })
