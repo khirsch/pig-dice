09beec004071759fd31c0bfe0e98d4b23779b5b5
Katrina Hirsch and Mark Lawson <katrina@>
2016-08-24 11:14:59 -0700

    add user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -9,46 +9,85 @@ function Player(score) {
   this.score = score;
 }
 
-var turnTotal = 0;
+var turnTotal, game, player1, player2, winner;
 
 function startGame() {
-  var game = new Game("player1", false);
-  var player1 = new Player(0);
-  var player2 = new Player(0);
+  player1 = new Player(0);
+  player2 = new Player(0);
+  game = new Game(player1, false);
+  turnTotal = 0;
 }
 
 function endGame() {
-
-}
-
-function roll() {
-  var roll = Math.floor(Math.random()*6)+1;
-  if (roll > 1) {
-    turnTotal += roll;
+  game.gameOver = true;
+  if (game.currentPlayer === player2) {
+    winner = "Player 1";
   } else {
-    turnTotal = 0;
-    if (game.currentPlayer === "player1") {
-      player1.score += turnTotal;
-      if (player1.score >= 100) {
-        endGame();
-      }
-      game.currentPlayer = "player2";
-    } else {
-      player2.score += turnTotal;
-      game.currentPlayer = "player1";
-      if (player2.score >= 100) {
-        endGame();
-      }
-    }
+    winner = "Player 2";
   }
 }
 
-function updatePlayerScore() {
+function endTurn() {
+  game.currentPlayer.score += turnTotal;
+  var currentScore = game.currentPlayer.score;
 
+  if (game.currentPlayer === player1) {
+    game.currentPlayer = player2;
+  } else {
+    game.currentPlayer = player1;
+  }
+
+  if (currentScore >= 10) {
+    endGame();
+  }
+
+  turnTotal = 0;
+  return currentScore;
+}
+
+function roll() {
+  var number = Math.floor(Math.random()*6)+1;
+  if (number > 1) {
+    turnTotal += number;
+  } else {
+    turnTotal = 0;
+    endTurn();
+  }
+  return number;
 }
 
 // user interface logic
+
 $(function() {
+  startGame();
 
+  $("#roll").click(function() {
+    var rollValue = roll();
+    $("#rollValue").text(rollValue);
+    $("#turnTotal").text(turnTotal);
+  });
 
-})
+  $("#hold").click(function() {
+    var currentScore = endTurn();
+
+    if (game.currentPlayer === player2) {
+      $("#score1").text(currentScore);
+    } else {
+      $("#score2").text(currentScore);
+    }
+
+    if (game.gameOver) {
+      $("#winner").text(winner + " wins!")
+    }
+  });
+
+  $("#restart").click(function() {
+    startGame();
+    $("#score1").text(player1.score);
+    $("#score2").text(player2.score);
+    $("#turnTotal").text(turnTotal);
+    $("#rollValue").text("");
+    $("#winner").text("");
+  });
+
+});
